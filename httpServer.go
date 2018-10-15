e5f8fa4c1cf3f5a9b0fa8b0aa91d556309af66e1
sharmasandeepkr <student.sandeepsharma@outlook.com>
2018-10-16 01:27:05 +0530

    gir

--- httpServer.go
+++ httpServer.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"mongoClient"
 	"net/http"
 	"os"
 	"signin"
@@ -11,7 +10,6 @@ import (
 	"structData"
 
 	"github.com/gorilla/mux"
-	"google.golang.org/appengine"
 )
 
 var SinT *template.Template
@@ -42,12 +40,12 @@ var SinT *template.Template
 
 func init() {
 	sinit.Init()
-	err := mongoClient.MyDB.ConnectDB()
-	if err != nil {
-		os.Exit(132)
-	}
-	// var err error
-	SinT, err = template.ParseFiles("sin_templates/sin_home.html", "sin_templates/sin_nav.html", "sin_templates/sin_farmSmallDevices.html", "sin_templates/sin_optedServices.html", "sin_templates/sin_suggestions.html", "sin_templates/sin_statistics.html", "sin_templates/sin_farmLargeDevices.html")
+	// err := mongoClient.MyDB.ConnectDB()
+	// if err != nil {
+	// 	os.Exit(132)
+	// }
+	var err error
+	SinT, err = template.ParseFiles("sin_templates/sin_home.html", "sin_templates/sin_nav.html", "sin_templates/sin_farmSmallDevices.html", "sin_templates/sin_optedServices.html", "sin_templates/sin_suggestions.html", "sin_templates/sin_statistics.html", "sin_templates/sin_farmLargeDevices.html", "sin_templates/sin_Products.html")
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(999)
@@ -55,32 +53,19 @@ func init() {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// ses := mongoClient.MyDB.Session
-	// myhandlerSes := ses.Copy()
-	// defer myhandlerSes.Close()
-	// col := myhandlerSes.DB("girgoras").C("sinHome")
-	// genHomeData := structData.SinHome{}
-	// err := col.Find(bson.M{"ucred.name": "string `bson:name`"}).One(&genHomeData)
+	// err = sinit.Templates["home"].ExecuteTemplate(w, "home", &genHomeData)
 	// if err != nil {
-	// 	os.Exit(12)
+	// 	os.Exit(111)
+	// 	//http.Error(w, "execution fails", 401)
 	// }
-	err := sinit.Templates["home"].ExecuteTemplate(w, "home", "&genHomeData")
+	err := SinT.ExecuteTemplate(w, "sin_home", &structData.SinHomeInstance)
 	if err != nil {
-		os.Exit(111)
-		//http.Error(w, "execution fails", 401)
+		http.Redirect(w, r, "/home", http.StatusTemporaryRedirect)
 	}
 }
 func hsin(w http.ResponseWriter, r *http.Request) {
 	var sinData structData.SinHome
 	sinData = structData.SinHomeInstance
-	// ses := mongoClient.MyDB.Session
-	// myhandlerSes := ses
-	// // defer myhandlerSes.Close()
-	// col := myhandlerSes.DB("girgoras").C("sinHome")
-	// err := col.Find(bson.M{"ucred.name": "string `bson:name`"}).One(&sinData)
-	// if err != nil {
-	// 	os.Exit(12)
-	// }
 	err := SinT.ExecuteTemplate(w, "sin_home", &sinData)
 	if err != nil {
 		http.Redirect(w, r, "/home", http.StatusTemporaryRedirect)
@@ -94,21 +79,21 @@ func decide(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	fmt.Println("i am starting with all due permission to Gau Maa!")
-	defer mongoClient.MyDB.Session.Close()
+	// defer mongoClient.MyDB.Session.Close()
 	// h := adapt(http.HandlerFunc(homeHandler), withDB(mongoClient.MyDB))
 	r := mux.NewRouter().StrictSlash(true)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	r.Handle("/", signin.Hsin(http.HandlerFunc(decide)))
+	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/auth/gplus", signin.HandleLogin)
 	r.Handle("/auth/gplus/callback", signin.HandlerCallback(http.HandlerFunc(signin.HandleCallback)))
 	r.HandleFunc("/sin", hsin)
 	// r.Handle("/home", context.ClearHandler(h))
 	r.HandleFunc("/home", homeHandler)
 	http.Handle("/", r)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
-		_ = fmt.Errorf("oh my god%v", err)
+		fmt.Print(fmt.Errorf("oh my god%v", err))
+		os.Exit(11)
 	}
-	appengine.Main()
 
 }
--- static/main.js
+++ static/main.js
@@ -1,6 +1,26 @@
 
+ function toggleSuggestions () { 
+    if ($('.suggestionRowClass').hasClass("d-none")) {
+        $("#txtSuggest").html('<mark style="color:`red`;">Click To Hide Suggested Services</mark>');
+            $(".suggestionRowClass").removeClass("d-none");
+            $(".suggestionRowClass").removeClass("d-lg-none");
+            $(".suggestionRowClass").removeClass("d-md-none");
+            $(".suggestionRowClass").removeClass("d-sm-none");
+        
+    }
+    else {
+            $("#txtSuggest").html('<mark style="color:`#31a2b8`;">Click For Services Suggestions</mark>');
+            $(".suggestionRowClass").addClass("d-none");
+            $(".suggestionRowClass").addClass("d-lg-none");
+            $(".suggestionRowClass").addClass("d-md-none");
+            $(".suggestionRowClass").addClass("d-sm-none");            
+
+    }
+ }
 
 window.onload= function(){
+   
+    
   $("#suggestionButton").click(function(){
       var src= $("#suggestionButton").attr("src");
       if (src == "./static/down.png"){
@@ -9,12 +29,8 @@ window.onload= function(){
       }
       else {
           $("#suggestionButton").attr('src', './static/down.png')
-          var suggestionsCount= $.get("/userHASH/sugestionsCount", data,
-              function (data, textStatus, jqXHR) {
-                  alert(data)
-              }
-          );
-          
+                    
       }
   })  
 }
+
